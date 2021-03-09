cf836945b759835dad6100826bb82bba91067c60
bio7 <marcel.au@web.de>
2021-03-09 17:16:34 +0100

    Changed.

--- com.eco.bio7.image/src/ij/plugin/ChannelArranger.java
+++ com.eco.bio7.image/src/ij/plugin/ChannelArranger.java
@@ -70,6 +70,7 @@ public class ChannelArranger implements PlugIn, TextListener {
 		ImagePlus imp2 = run(imp, newOrder2);
 		/*Changed for Bio7! It seems necessary for MacOSX only!*/
 		imp.hide();
+		
 		imp2.copyAttributes(imp);
 		if (location!=null)
 			ImageWindow.setNextLocation(location);
