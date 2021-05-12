29c9dd93ed03f6e675f9b52b49f7a34e5ad755cc
Bio7 <marcel.au@web.de>
2021-05-12 12:35:38 +0200

    Updated ImageJ Plugin to version 1.53j43

--- com.eco.bio7.image/src/ij/ImageJ.java
+++ com.eco.bio7.image/src/ij/ImageJ.java
@@ -97,7 +97,7 @@ public class ImageJ extends Frame implements ActionListener, MouseListener, KeyL
 	 * string.
 	 */
 	public static final String VERSION = "1.53j";
-	public static final String BUILD = "35";
+	public static final String BUILD = "43";
 	/*Changed for Bio7!*/
 	public static Color backgroundColor;
 	/** SansSerif, 12-point, plain font. */
