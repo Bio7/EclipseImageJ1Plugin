3ad56a4db6283a008bda11c4c38d922034afa7f1
 bio7 <marcel.au@web.de>
2022-01-16 11:26:24 +0100

    Changed.

--- com.eco.bio7.image/src/ij/io/Opener.java
+++ com.eco.bio7.image/src/ij/io/Opener.java
@@ -42,6 +42,7 @@ public class Opener {
 	private static boolean openUsingPlugins;
 	private static boolean bioformats;
 	private String url;
+ 	private boolean useHandleExtraFileTypes;
 	private static boolean errorMessage;// Changed for Bio7!
     
 	
@@ -100,8 +101,10 @@ public class Opener {
 		ImagePlus imp = null;
 		if (path.endsWith(".txt"))
 			this.fileType = JAVA_OR_TEXT;
-		else
+		else {
+			useHandleExtraFileTypes = true;
 			imp = openImage(path);
+		}
 		if (imp==null && isURL)
 			return;
 		if (imp!=null) {
@@ -373,8 +376,11 @@ public class Opener {
 					return null;
 			case UNKNOWN: case TEXT:
 				imp = null;
-				if (!name.endsWith(".lsm"))  // use LSM_Reader to opem .lsm files
+				if (name.endsWith(".lsm"))
+					useHandleExtraFileTypes = true; // use LSM_Reader to opem .lsm files
+				if (!useHandleExtraFileTypes)
 					imp = openUsingBioFormats(path);
+				useHandleExtraFileTypes = false;
 				if (imp!=null)
 					return imp;
 				else
