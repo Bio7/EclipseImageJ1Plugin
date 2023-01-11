6600f3e4bfa3285ee6afe6757fe514ce1fa2e0f9
Bio7 <marcel.au@web.de>
2023-01-11 16:50:45 +0100

    Updated ImageJ to version 1.54b (5)

--- com.eco.bio7.image/src/ij/ImageJ.java
+++ com.eco.bio7.image/src/ij/ImageJ.java
@@ -96,8 +96,8 @@ public class ImageJ extends Frame implements ActionListener, MouseListener, KeyL
 	 * Plugins should call IJ.getVersion() or IJ.getFullVersion() to get the version
 	 * string.
 	 */
-	public static final String VERSION = "1.54a";
-	public static final String BUILD = ""; //37
+	public static final String VERSION = "1.54b";
+	public static final String BUILD = ""; //5
 	public static Color backgroundColor;
 	/** SansSerif, 12-point, plain font. */
 	public static final Font SansSerif12 = new Font("SansSerif", Font.PLAIN, 12);
@@ -1030,7 +1030,7 @@ public class ImageJ extends Frame implements ActionListener, MouseListener, KeyL
 				IJ.open(file.getAbsolutePath());
 			}
 		}
-		if (IJ.debugMode && IJ.getInstance() == null)
+		if (IJ.debugMode && IJ.getInstance()==null && !GraphicsEnvironment.isHeadless())
 			new JavaProperties().run("");
 		if (noGUI)
 			System.exit(0);
