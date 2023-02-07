c467360f8497f414e5f81517388598a9b9d0ab40
Bio7 <marcel.au@web.de>
2023-02-07 16:54:49 +0100

    Updated the ImageJ plugin to version 1.54c7.

--- com.eco.bio7.image/src/ij/ImageJ.java
+++ com.eco.bio7.image/src/ij/ImageJ.java
@@ -97,7 +97,7 @@ public class ImageJ extends Frame implements ActionListener, MouseListener, KeyL
 	 * string.
 	 */
 	public static final String VERSION = "1.54c";
-	public static final String BUILD = "5";
+	public static final String BUILD = "7";
 	public static Color backgroundColor;
 	/** SansSerif, 12-point, plain font. */
 	public static final Font SansSerif12 = new Font("SansSerif", Font.PLAIN, 12);
